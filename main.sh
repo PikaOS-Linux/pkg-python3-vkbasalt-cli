48d2ff4df725a69168445428e43c4334a11d7781
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-06-26 13:07:58 +0300

    first commit

---
+++ main.sh
@@ -0,0 +1,15 @@
+# Clone Upstream
+git clone https://gitlab.com/TheEvilSkeleton/vkbasalt-cli -b 3.1.1
+cp -rvf ./debian ./vkbasalt-cli/
+cd ./vkbasalt-cli
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
